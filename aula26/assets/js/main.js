7d1932d8c80deb82fbab7df8ee0ebaec3205583b
brenofreitast <brenofreitast@gmail.com>
2025-08-04 17:13:29 -0300

    UPDATE IMC 2

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,7 +1,30 @@
 function meuEscopo() {
 
-    
+    const form = document.querySelector('#form');
 
- 
+    form.addEventListener('submit', function (evento) {
+        evento.preventDefault();
+        const inputPeso = evento.target.querySelector('#peso');
+        const inputAltura = evento.target.querySelector('#altura');
+
+        const peso = Number(inputPeso.value);
+        const altura = Number(inputAltura.value);
+    });
+
+    function criaP() {
+        const p = document.createElement('p'); // Criou elemento <p> </p>
+        return p;
+
+    }
+
+    function setResultado(msg) {
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = '';
+        
+        const p = criaP();
+        
+    }
+
+
 }
 meuEscopo();
